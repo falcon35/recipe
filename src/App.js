9880360dc023f8ad95cb95fda8185ff1e96f1431
Aram <amehrabyan111@gmail.com>
2019-11-17 22:49:55 +0400

    Recipe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import RecipeDetails from './components/RecipeDetails'
+import {recipes} from './tempList'
+
+export default class App extends Component {
+  state={
+    recipes:recipes,
+    url:"https://www.food2fork.com/api/search?key=5d29b243270eda16f2a50184b84e4fc9",
+    base_url:"https://www.food2fork.com/api/search?key=5d29b243270eda16f2a50184b84e4fc9",
+    detail_id: 35374,
+    pageIndex:1,
+    search:'',
+    query:"&q=",
+    error:''
+  }
+async getRecipes(){
+    try{
+      const data= await fetch(this.state.url);
+      const jsonData=await data.json();
+      if(jsonData.recipes.length===0){
+        this.setState( ()=> 
+        {  return  {error:"sorry but your search didn't return any result"}
+           
+          }
+        )
+      } else{
+      this.setState(
+        {
+          recipes:jsonData.recipes
+        }
+      )
+    }
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+  componentDidMount(){
+    this.getRecipes()
+  }
+  
+ displayPage=index=>{
+   switch(index){
+     case 1:  
+       return <RecipeList recipes={this.state.recipes} handleDetails={this.handleDetails} value={this.state.search} 
+       handleChange={this.handleChange} handleSubmit={this.handleSubmit} error={this.state.error}/>;
+       case 0: 
+       return <RecipeDetails id={this.state.detail_id} handleIndex={this.handleIndex}/>;
+   }
+ }
+ handleIndex=index=>{
+   this.setState({
+     pageIndex:index
+   });
+ }
+ handleDetails=(index,id)=>{
+  this.setState({
+    pageIndex:index,
+    detail_id: id
+  });
+}
+handleChange=e=>{
+  this.setState(
+    {
+      search: e.target.value
+    }
+  )
+}
+handleSubmit=(e)=>{
+  e.preventDefault();
+  const {base_url,query,search}=this.state;
+  this.setState(
+    ()=>{
+      return {url:`${base_url}${query}${search}`,search:""}
+      
+    },()=>this.getRecipes()
+  )
 }
 
-export default App;
+  render() {
+    return (
+      <React.Fragment>
+       {this.displayPage(this.state.pageIndex)}
+        
+      </React.Fragment>
+    )
+  }
+}
+
---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class RecipeSearch extends Component {
+    render() {
+        const {value,handleChange,handleSubmit}=this.props;
+        return (
+            <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+                <h1 className="text-slanted text-capitalize"> 
+                search for recipe with<strong className="text-danger">
+                    Food2Fork</strong></h1>
+                    <form className="mt-4" onSubmit={handleSubmit}>
+                <label htmlFor="search" className="text-capitalize">type recipes separate by comma</label>
+                    <div className="input-group">
+                    <input type="text" className="form-control" name="search" placeholder="chicken,onions,carrot" value={value} onChange={handleChange}/>
+                    <div className="input-group-append">
+                    <button type="submit" className="input-group-text bg-primary text-white">
+                    <i className="fa fa-search"></i>
+                    </button>
+                    </div>
+                    </div>
+                    </form>
+                    </div>
+                </div>
+
+            </div>
+            </React.Fragment>
+        )
+    }
+}
